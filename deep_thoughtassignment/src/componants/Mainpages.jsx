26767470bdfe8e6daf40fbf1a60d015e7e88c5cd
Hilton-Borah <hiltonborah123@gmail.com>
2023-03-27 01:03:27 +0530

    all done

--- deep_thoughtassignment/src/componants/Mainpages.jsx
+++ deep_thoughtassignment/src/componants/Mainpages.jsx
@@ -9,24 +9,35 @@ import Thredbuilder from './Thredbuilder';
 import Structure from './Structure';
 import FourSA from './FourSA';
 import { HiArrowNarrowLeft, HiArrowNarrowRight } from "react-icons/hi"
+import { FaPlus } from "react-icons/fa";
+import {ImCross} from "react-icons/im"
 
 const Mainpages = () => {
   const [data, setData] = useState({});
-  const [sidebar , setSidebar] = useState(true)
+  const [sidebar, setSidebar] = useState(true)
+  const [sidebartwo, setSidebartwo] = useState(true)
 
 
   // your json server api showing cors policy arror so I am using my own cyclic api using just you json database code 
 
 
-  
-  const handleClickone = ()=>{
+
+  const handleClickone = () => {
     setSidebar(false)
   }
-  
-  const handleClicktwo = ()=>{
+
+  const handleClicktwo = () => {
     setSidebar(true)
   }
-  
+
+  const hanldeClickthree = () =>{
+    setSidebartwo(false)
+  }
+
+  const hanldeClickfour = () =>{
+    setSidebartwo(true)
+  }
+
   useEffect(() => {
     axios.get("https://splendid-shoulder-pads-mite.cyclic.app/deepthoughtassignemnt")
       .then((res) => {
@@ -44,50 +55,50 @@ const Mainpages = () => {
       <div className='mainbodyhomepage' >
         <div>
           {
-           sidebar ?
-        <div className='leftsidebar'>
-          <div>
-            <div  onClick={handleClickone}>
-              <HiArrowNarrowRight/>
-            </div>
-          </div>
-          
-          <div>
-            <div>
-              <div>1</div>
-            </div>
-          </div>
-        </div> :
-        <div className='leftsidebarJourney'>
-          <div>
-            <div>Journey board</div>
-            <div onClick={handleClicktwo}>
-              <HiArrowNarrowLeft />
-            </div>
-          </div>
+            sidebar ?
+              <div className='leftsidebar'>
+                <div>
+                  <div onClick={handleClickone}>
+                    <HiArrowNarrowRight />
+                  </div>
+                </div>
 
-          <div>
-            <div>
-              <div>
-                <li className='firstTitle'>{data && data.data && data.data.tasks && data.data.tasks[0].task_title}</li>
-                {
-                  data && data.data && data.data.tasks && data.data.tasks[0].assets.map((el, index) => {
-                    return (
-                      <div key={el.id} className="titlelist">
-                        <li>{el.asset_title}</li>
-                      </div>
-                    )
-                  })
-                }
+                <div>
+                  <div>
+                    <div>1</div>
+                  </div>
+                </div>
+              </div> :
+              <div className='leftsidebarJourney'>
+                <div>
+                  <div>Journey board</div>
+                  <div onClick={handleClicktwo}>
+                    <HiArrowNarrowLeft />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <div>
+                      <li className='firstTitle'>{data && data.data && data.data.tasks && data.data.tasks[0].task_title}</li>
+                      {
+                        data && data.data && data.data.tasks && data.data.tasks[0].assets.map((el, index) => {
+                          return (
+                            <div key={el.id} className="titlelist">
+                              <li>{el.asset_title}</li>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
           }
         </div>
 
 
-        <div  onClick={handleClicktwo}>
+        <div onClick={handleClicktwo}>
           <Heading headingtitle={data && data.data && data.data.title} secondheading={data && data.data && data.data.tasks && data.data.tasks[0].task_title} paragraph={data && data.data && data.data.tasks && data.data.tasks[0].task_description} />
           <div className='mainBody'>
             {
@@ -101,9 +112,29 @@ const Mainpages = () => {
             }
           </div>
         </div>
-        <div>
-          second sidebar
+
+      <div>
+        {
+          sidebartwo ? 
+        <div className='rightsidebar'>
+          <div>
+            <div className='firstright' onClick={hanldeClickthree}><ImCross/></div>
+            <div>N o t i c e</div>
+            <div>B o a r d</div>
+          </div>
+          <div></div>
+        </div> : 
+        <div className='rightsidebartwo'>
+          <div></div>
+          <div>
+            <div className='firstrighttwo' onClick={hanldeClickfour}><FaPlus/></div>
+            <div>N o t i c e</div>
+            <div>B o a r d</div>
+          </div>
         </div>
+        }
+      </div>
+
       </div>
 
     </div>
